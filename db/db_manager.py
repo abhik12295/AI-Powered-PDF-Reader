2c5d2d6e5ef942f527e08de5181f8d3b9c4b4c58
Abhishek Kumar <abhik12295@gmail.com>
2025-02-26 11:34:12 -0600

    database setting up

--- db/db_manager.py
+++ db/db_manager.py
@@ -9,4 +9,35 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def get_user_pdfs(user_id):
-    pass
+    # fetch all pdfs for user
+    response = supabase.table("user_pdfs").select("*").eq("user_id", user_id).execute()
+    return response.data if response.data else []
+
+def get_user_pdf(user_id, pdf_hash):
+    #fetch specific pdf by hash for a user
+    response = supabase.table("user_pdfs").select("*").eq("user_id", user_id).eq("pdf_hash", pdf_hash).execute()
+    return response.data[0] if response.data else None
+
+def save_user_pdfs(user_id, pdf_name, pdf_hash, extracted_text, summary):
+    existing_pdfs = get_user_pdf(user_id)
+    if len(existing_pdfs)>=5:
+        return False
+    
+    data = {
+        "user_id": user_id,
+        "pdf_name": pdf_name,
+        "pdf_hash": pdf_hash,
+        "extracted_text": extracted_text,
+        "summary": summary,
+        "created_at": "now()"
+    }
+    response = supabase.table("user_pdfs").insert(data).execute()
+    return response.data is not None
+
+def delete_user_pdf(user_id, pdf_hash):
+    supabase.table("user_pdfs").delete().eq("user_id", user_id).eq("pdf_hash", pdf_hash).execute()
+
+def rename_user_pdf(user_id, pdf_hash, new_name):
+    supabase.table("user_pdfs").update({"pdf_name": new_name}).eq("user_id", user_id).eq("pdf_hash", pdf_hash).execute()
+    
+
